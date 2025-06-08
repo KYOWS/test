9bab22a2b7cc59e11895d981dd46d1a3d1f0287c
Wallison Santos <110735811+KYOWS@users.noreply.github.com>
2025-06-08 16:09:59 -0300

    Update install_test.sh

--- install_test.sh
+++ install_test.sh
@@ -24,8 +24,8 @@ spinner() {
     local pid=$1
     local delay=0.1
     local spinstr='|/-\'
-    #while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-    for i in {1..100}; do # Simula 5 iterações rápidas
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    #for i in {1..100}; do # Simula 5 iterações rápidas
         local temp=${spinstr#?}
         printf " [%c]  " "$spinstr"
         local spinstr=$temp${spinstr%"$temp"}
