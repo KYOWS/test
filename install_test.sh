0f0b7e036508ee1364d75dd7e1376a292e9273b5
Wallison Santos <110735811+KYOWS@users.noreply.github.com>
2025-06-08 20:57:15 -0300

    Update install_test.sh

--- install_test.sh
+++ install_test.sh
@@ -87,7 +87,7 @@ check_docker_installed() {
 #######################################################
 
 install_docker_function() {
-    sudo apt-get update && \
+    sudo apt-get update -y && \
     sudo apt-get install ca-certificates curl -y && \
     sudo install -m 0755 -d /etc/apt/keyrings && \
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && \
@@ -96,7 +96,7 @@ install_docker_function() {
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
-    sudo apt update -y && sudo apt upgrade -y
+    sudo apt-get update -y
 }
 
 #########################
