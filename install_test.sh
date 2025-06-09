c724ec8d8f01d0eb6c184b13249d4a77780d2255
Wallison Santos <110735811+KYOWS@users.noreply.github.com>
2025-06-08 21:21:22 -0300

    Update install_test.sh

--- install_test.sh
+++ install_test.sh
@@ -96,7 +96,8 @@ install_docker_function() {
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
-    sudo apt-get update -y
+    sudo apt-get update -y && \
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
 #########################
