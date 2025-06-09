30742c318f2bd9d78c5057fa6b8c5b5920339cc1
Wallison Santos <110735811+KYOWS@users.noreply.github.com>
2025-06-09 19:08:35 -0300

    Update install_test.sh

--- install_test.sh
+++ install_test.sh
@@ -438,18 +438,18 @@ services:
     labels:
       - "traefik.enable=true"
     # Roteador e Serviço para a interface principal do Portainer (porta 9000)
-     - "traefik.http.routers.portainer.rule=Host(\'$portainer_domain\')"
-     - "traefik.http.routers.portainer.tls=true"
-     - "traefik.http.routers.portainer.tls.certresolver=lets-encrypt"
-     - "traefik.http.services.portainer-main.loadbalancer.server.port=9000" # Define um serviço Traefik chamado 'portainer-main'
-     - "traefik.http.routers.portainer.service=portainer-main" # O roteador 'portainer' usa o serviço 'portainer-main'
+      - "traefik.http.routers.portainer.rule=Host(\'$portainer_domain\')"
+      - "traefik.http.routers.portainer.tls=true"
+      - "traefik.http.routers.portainer.tls.certresolver=lets-encrypt"
+      - "traefik.http.services.portainer-main.loadbalancer.server.port=9000" # Define um serviço Traefik chamado 'portainer-main'
+      - "traefik.http.routers.portainer.service=portainer-main" # O roteador 'portainer' usa o serviço 'portainer-main'
     # Roteador e Serviço para o endpoint Edge do Portainer (porta 8000)
-     - "traefik.http.routers.edge.rule=Host(\'$edge_domain\')"
-     - "traefik.http.routers.edge.entrypoints=websecure"
-     - "traefik.http.services.portainer-edge.loadbalancer.server.port=8000" # Define um serviço Traefik chamado 'portainer-edge'
-     - "traefik.http.routers.edge.service=portainer-edge" 
-     - "traefik.http.routers.edge.tls.certresolver=lets-encrypt"
-     - "traefik.docker.network=web"
+      - "traefik.http.routers.edge.rule=Host(\'$edge_domain\')"
+      - "traefik.http.routers.edge.entrypoints=websecure"
+      - "traefik.http.services.portainer-edge.loadbalancer.server.port=8000" # Define um serviço Traefik chamado 'portainer-edge'
+      - "traefik.http.routers.edge.service=portainer-edge" 
+      - "traefik.http.routers.edge.tls.certresolver=lets-encrypt"
+      - "traefik.docker.network=web"
     logging:
       options:
         max-size: "10m"
