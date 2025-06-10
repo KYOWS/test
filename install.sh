ca4d8bf661b87086b96f8ca37cf96afed1cf5435
Wallison Santos <110735811+KYOWS@users.noreply.github.com>
2025-06-10 20:36:07 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@
   [http.middlewares.redirect-www-to-main.redirectregex]
       permanent = true
       regex = "^https?://www\\.(.+)"
-      replacement = "https://$${1}"
+      replacement = "https://${1}"
 
 # NOVO: Definição do middleware de segurança de cabeçalhos HTTP
 [http.middlewares.securityHeaders.headers]
