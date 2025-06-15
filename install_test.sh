bf493c6ed174fe076239f4f2035ded33f1b68b62
Wallison Santos <110735811+KYOWS@users.noreply.github.com>
2025-06-15 10:20:25 -0300

    Update install_test.sh

--- install_test.sh
+++ install_test.sh
@@ -101,17 +101,26 @@ check_docker_installed() {
 #######################################################
 
 install_docker_function() {
+    # Atualizar repositórios
     sudo apt-get update -y && \
-    sudo apt-get install ca-certificates curl -y && \
+    # Instalar dependências
+    sudo apt-get install ca-certificates curl gnupg lsb-release -y && \
+    # Criar diretório para chaves
     sudo install -m 0755 -d /etc/apt/keyrings && \
+    # Baixar chave GPG do Docker
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && \
+    # Definir permissões da chave
     sudo chmod a+r /etc/apt/keyrings/docker.asc && \
+    # Adicionar repositório Docker
     echo \
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+      # Atualizar novamente e instalar Docker
     sudo apt-get update -y && \
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && \
+    # Adicionar usuário atual ao grupo docker
+    sudo usermod -aG docker $USER
 }
 
 #########################
