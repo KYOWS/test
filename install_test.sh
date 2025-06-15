168d98ab286441fbb088788057ee6d4accde2142
Wallison Santos <110735811+KYOWS@users.noreply.github.com>
2025-06-15 12:14:54 -0300

    Update install_test.sh

--- install_test.sh
+++ install_test.sh
@@ -460,12 +460,7 @@ services:
     logging:
       options:
         max-size: "10m"
-        max-file: "3"
-    #healthcheck:
-    #  test: ["CMD", "traefik", "healthcheck"]
-    #  interval: 30s
-    #  timeout: 10s
-    #  retries: 3
+        max-file: "3"    
 
   portainer:
     image: portainer/portainer-ce:latest
@@ -501,12 +496,7 @@ services:
     logging:
       options:
         max-size: "10m"
-        max-file: "3"
-    #healthcheck:
-    #  test: ["CMD-SHELL", "curl -f http://localhost:9000/api/status || exit 1"]
-    #  interval: 30s
-    #  timeout: 10s
-    #  retries: 3
+        max-file: "3"   
 
 networks:
   web:
